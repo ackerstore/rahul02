5e05a9fe0ecc3935e773ade4a5091dc8e9c40e82
ackerstore <geetanshukachhawaha@gmail.com>
2026-07-20 19:59:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import threading
+from Autoshopify import app
+from bot import start_bot
+
+def run_api():
+    app.run(host="0.0.0.0", port=8080)
+
+threading.Thread(target=run_api).start()
+
+start_bot()
